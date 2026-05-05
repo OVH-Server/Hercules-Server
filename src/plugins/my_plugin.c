99aba10467caa99e5c66c1f85324c00f00e2e01b
Ycaro02 <ycaro02@hotmail.fr>
2026-05-05 09:33:38 +0000

    Work on essence account system, working NPC side, removed in plugin side, just need to update account balance in plugin

--- src/plugins/my_plugin.c
+++ src/plugins/my_plugin.c
@@ -26,12 +26,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct my_plugin_data {
+typedef struct drop_protector_data {
     int killer_char_id;
     int item_id;
     int mob_id;
     int is_card;
-};
+    struct map_session_data *sd;
+} DropProtectorData ;
 
 HPExport struct hplugin_info pinfo = {
     "my_plugin",
@@ -40,28 +41,37 @@ HPExport struct hplugin_info pinfo = {
     HPM_VERSION,
 };
 
-int get_npc_variable_data(int char_id, char *variable_to_read) {
-    int int_data = 0;
-    
-    if (SQL_ERROR == SQL->Query(map->mysql_handle, 
-        "SELECT `value` FROM `char_reg_num_db` WHERE `char_id` = '%d' AND `key` = '%s' AND `index` = 0", 
-        char_id, variable_to_read)) {
-        Sql_ShowDebug(map->mysql_handle);
-        ShowError("drop_protector: Error on request get drop_protector_monster_id\n");
-        return 0;
-    }
-    if (SQL_SUCCESS == SQL->NextRow(map->mysql_handle)) {
-        char *data;
-        SQL->GetData(map->mysql_handle, 0, &data, NULL);
-        int_data = atoi(data);
-    }
-    SQL->FreeResult(map->mysql_handle);
-    ShowInfo("drop_protector: Get %s ==> %d for char id %d\n", variable_to_read, int_data, char_id);
+int get_db_data_from_char_id(struct map_session_data *sd, char *variable_to_read) {
+    int64 var_index = script->add_str(variable_to_read);
+    int int_data = pc->readregistry(sd, var_index);
+    ShowInfo("drop_protector: Get %s ==> %d for char id %d\n", variable_to_read, int_data, sd->status.char_id);
     return (int_data);
 }
 
-int check_drop_protector(int char_id, int mob_id, struct my_plugin_data *mydata) {
+void set_db_data_for_char_id(struct map_session_data *sd, char *variable_name, int value) {
+    int64 var_index = script->add_str(variable_name);
+    pc->setregistry(sd, var_index, value);
+    ShowInfo("drop_protector: Set %s ==> %d for char id %d\n", variable_name, value, sd->status.char_id);
+}
+
+int check_drop_protector(int char_id, int mob_id, DropProtectorData *mydata) {
     return (mydata->mob_id == mob_id) ? 1 : 0;
+}
+
+struct map_session_data *get_map_session_data_from_block_list(struct block_list *src_data) {
+    if (src_data->type == BL_PC) {
+        return (BL_CAST(BL_PC, src_data));
+    } 
+    else if (src_data->type == BL_PET) {
+        return (BL_UCAST(BL_PET, src_data)->msd);
+    } 
+    else if (src_data->type == BL_MER) {
+        return (BL_UCAST(BL_MER, src_data)->master);
+    }
+    else if (src_data->type == BL_HOM) {
+        return (BL_UCAST(BL_HOM, src_data)->master);
+    }
+    return (NULL);
 }
 
 int myplugin_custom_mob_dead_pre(struct mob_data **md, struct block_list **src, int *type) {
@@ -75,19 +85,7 @@ int myplugin_custom_mob_dead_pre(struct mob_data **md, struct block_list **src, 
 
     src_data = *src;
 
-    /* Get player struct from block list */
-    if (src_data->type == BL_PC) {
-        sd = BL_CAST(BL_PC, src_data);
-    } 
-    else if (src_data->type == BL_PET) {
-        sd = BL_UCAST(BL_PET, src_data)->msd;
-    } 
-    else if (src_data->type == BL_MER) {
-        sd = BL_UCAST(BL_MER, src_data)->master;
-    }
-    else if (src_data->type == BL_HOM) {
-        sd = BL_UCAST(BL_HOM, src_data)->master;
-    }
+    sd = get_map_session_data_from_block_list(src_data);
 
     // if we had player struct + mob data apply logic
     if (sd && mob_data) {
@@ -98,13 +96,19 @@ int myplugin_custom_mob_dead_pre(struct mob_data **md, struct block_list **src, 
                  sd->status.name, char_id, mob_id);
 
 
-        struct my_plugin_data *mydata;
-        CREATE(mydata,struct my_plugin_data,1);
-        mydata->mob_id = get_npc_variable_data(char_id, "drop_protector_monster_id");
-        mydata->item_id = get_npc_variable_data(char_id, "drop_protector_item_id");
-        mydata->is_card = get_npc_variable_data(char_id, "drop_protector_item_is_card");
+        DropProtectorData *mydata;
+        CREATE(mydata,DropProtectorData,1);
+        if (!mydata) {
+            ShowWarning("drop_protector: Can't alloc DropProtectorData struct\n");
+            return 0;
+        }
+
+        mydata->mob_id = get_db_data_from_char_id(sd, "drop_protector_monster_id");
+        mydata->item_id = get_db_data_from_char_id(sd, "drop_protector_item_id");
+        mydata->is_card = get_db_data_from_char_id(sd, "drop_protector_item_is_card");
         mydata->killer_char_id = sd->status.char_id;
-        
+        mydata->sd = sd;
+        // script->set_reg
 
         if (check_drop_protector(char_id, mob_id, mydata)) {
             ShowInfo("drop_protector: DROP PROTECTOR ACTIVATED! Player %s has protection for Mob ID %d\n", 
@@ -136,8 +140,53 @@ int myplugin_custom_mob_dead_pre(struct mob_data **md, struct block_list **src, 
 
 // poring map @warp prt_fild08
 
+static int count_item_in_inventory(struct map_session_data *sd, int item_id) {
+    int count = 0;
+    int i;
+    
+    for (i = 0; i < sd->status.inventorySize; i++) {
+        if (sd->status.inventory[i].nameid == item_id) {
+            count += sd->status.inventory[i].amount;
+        }
+    }
+    
+    ShowInfo("drop_protector: Player %s has %d of item ID %d\n", 
+             sd->status.name, count, item_id);
+    return count;
+}
+
+static int remove_item_amount(struct map_session_data *sd, int item_id, int amount_to_remove) {
+    int i;
+
+    int remaining = amount_to_remove;
+
+
+    for (i = 0; i < sd->status.inventorySize && remaining > 0; i++) {
+        if (sd->status.inventory[i].nameid == item_id) {
+            int slot_amount = sd->status.inventory[i].amount;
+            int to_delete = (slot_amount > remaining) ? remaining : slot_amount;
+            
+            if (pc->delitem(sd, i, to_delete, 0, DELITEM_NORMAL, LOG_TYPE_SCRIPT) == 0) {
+                remaining -= to_delete;
+                ShowInfo("drop_protector: Removed %d of item ID %d from slot %d\n", 
+                         to_delete, item_id, i);
+            }
+        }
+    }
+    
+    if (remaining == 0) {
+        ShowInfo("drop_protector: Successfully removed %d of item ID %d\n", 
+                 amount_to_remove, item_id);
+        return 1;
+    } else {
+        ShowWarning("drop_protector: Could only remove %d of %d items\n", 
+                    amount_to_remove - remaining, amount_to_remove);
+        return 0;
+    }
+}
+
 void myplugin_custom_mob_item_drop_post(struct mob_data *md, struct item_drop_list *dlist, struct item_drop *ditem, int loot, int drop_rate, unsigned short flag) {
-    struct my_plugin_data *mydata;
+    DropProtectorData *mydata;
     struct item_drop *current;
     int count = 0;
 
@@ -148,24 +197,29 @@ void myplugin_custom_mob_item_drop_post(struct mob_data *md, struct item_drop_li
 
     ShowInfo("drop_protector: --- Full Drop List for Mob %d (Killer: %d) loot: %d\n", md->db->mob_id, mydata->killer_char_id, loot);
     
+    if (mydata->mob_id != md->db->mob_id) {
+        ShowInfo("drop_protector: Not targeted mob id return\n");
+        return ;
+    }
+
     current = dlist->item;
     while (current != NULL) {
-        struct item_data *id = itemdb->search(current->item_data.nameid);
-        
-        // ShowInfo("drop_protector: Drop #%d | ID: %d | Name: %s | Amount: %d\n", 
-        //             count,
-        //             current->item_data.nameid, 
-        //             id ? id->name : "Unknown",
-        //             current->item_data.amount);
         
         if (current->item_data.nameid == mydata->item_id) {
-            ShowInfo("drop_protector: ITEM DROPEEED\n");
+            ShowInfo("drop_protector: Item is drop update var\n");
+            set_db_data_for_char_id(mydata->sd, "drop_protector_item_is_dropped", 1); // maybe not mandatory
+            set_db_data_for_char_id(mydata->sd, "drop_protector_item_is_card", 0);
+            set_db_data_for_char_id(mydata->sd, "drop_protector_monster_id", -1);
+            set_db_data_for_char_id(mydata->sd, "drop_protector_item_id", -1);
+            int amount_to_remove = count_item_in_inventory(sd, 7836);
+            // update essence account
+            remove_item_amount(mydata->sd, 7836, amount_to_remove);
+            break ;
         }
 
         current = current->next;
         count++;
     }
-    ShowInfo("drop_protector: --- End of List (%d items) ---\n", count);
 }
 
 HPExport void plugin_init(void) {
@@ -189,3 +243,12 @@ HPExport void server_online(void) {
 HPExport void server_ready(void) {
     ShowInfo("drop_protector: Server is ready\n");
 }
+
+
+/* Debug to display loot */
+// struct item_data *id = itemdb->search(current->item_data.nameid);
+// ShowInfo("drop_protector: Drop #%d | ID: %d | Name: %s | Amount: %d\n", 
+//             count,
+//             current->item_data.nameid, 
+//             id ? id->name : "Unknown",
+//             current->item_data.amount);
